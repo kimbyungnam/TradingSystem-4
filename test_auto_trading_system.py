d0dac8e76cde0306256cdbfda3f5de655e78d352
kimbyungnam <kimbyungnam@ajou.ac.kr>
2026-07-03 12:51:21 +0900

    [test] buy

--- test_auto_trading_system.py
+++ test_auto_trading_system.py
@@ -36,3 +36,15 @@ def test_login(mocker: MockerFixture):
     # Assertion
     mock_brocker.login.assert_called_once_with("ID_NOT_IMPORTANT", "PW_NOT_IMPORTANT")
     pass
+
+
+def test_buy(mocker: MockerFixture):
+    # Arrange
+    sut = AutoTradingSystem()
+    mock_brocker = mocker.patch("auto_trading_system.AutoTradingSystem.stock_brocker")
+
+    # Action
+    sut.buy("Stock1", 1000, 2)
+
+    # Assertion
+    mock_brocker.buy.assert_called_once_with("Stock1", 1000, 2)
