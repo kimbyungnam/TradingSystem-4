ead3c0d7bdb8628e853810d530c789fbcfd8fc5e
kimbyungnam <kimbyungnam@ajou.ac.kr>
2026-07-03 12:44:11 +0900

    [feature] Define StockBrocker protocol

--- auto_trading_system.py
+++ auto_trading_system.py
@@ -1,3 +1,16 @@
+from typing import Protocol
+
+
+class StockBrocker(Protocol):
+    def login(self, id: str, pw: str): ...
+
+    def buy(self, stock_code: str, price: int, count: int): ...
+
+    def sell(self, stock_code: str, price: int, count: int): ...
+
+    def get_price(self, stock_code: str): ...
+
+
 class AutoTradingSystem:
     def select_stock_broker(self, broker_name):
         self.stock_broker = broker_name
@@ -5,6 +18,7 @@ class AutoTradingSystem:
     @property
     def stock_broker(self):
         return self._stock_broker
+
     @stock_broker.setter
     def stock_broker(self, broker_name):
         self._stock_broker = broker_name
