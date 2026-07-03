82713da2c01a8d0af0fe593d28ae84bd5c23cec4
kimbyungnam <kimbyungnam@ajou.ac.kr>
2026-07-03 13:42:28 +0900

    [test] buy_nice_timing_rising_trend_buy

--- test_auto_trading_system.py
+++ test_auto_trading_system.py
@@ -104,3 +104,16 @@ def test_buy_nice_timing_get_price_3_times(mocker: MockerFixture):
         ("sleep", 0.2),
         ("get_price", "Stock1"),
     ]
+
+
+def test_buy_nice_timing_rising_trend_buy(mocker: MockerFixture):
+    # Arrange
+    sut = AutoTradingSystem()
+    mock_brocker = mocker.patch("auto_trading_system.AutoTradingSystem.stock_brocker")
+    mock_brocker.get_price.return_value = [100, 200, 300]
+
+    # Action
+    sut.buy_nice_timing("Stock1", 1000)
+
+    # Assert
+    mock_brocker.buy.assert_called_once_with("Stock1", 300, 3)
