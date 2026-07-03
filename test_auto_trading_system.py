25c13decbdcc4d91850bee7b7cc3c2ad785642cf
hasup.lee <hasup.lee@samsung.com>
2026-07-03 13:36:29 +0900

    start test_get_price

--- test_auto_trading_system.py
+++ test_auto_trading_system.py
@@ -1,5 +1,5 @@
 from auto_trading_system import AutoTradingSystem
-
+from pytest_mock import MockerFixture
 
 def test_select_stock_broker():
     # Arrange
@@ -10,3 +10,16 @@ def test_select_stock_broker():
 
     # Assert
     assert sut.stock_broker == "kiwer"
+
+def test_get_price(mocker: MockerFixture):
+    # Arrange
+    sut = AutoTradingSystem()
+    mock_brocker = mocker.patch("auto_trading_system.AutoTradingSystem.stock_brocker")
+    mock_brocker.get_price.return_value = 2000
+
+    # Action
+    price = sut.get_price("Stock1")
+
+    # Assertion
+    assert price == 2000
+
