2387d4306448b0f3804c1f72de4df4e22e94566a
junwon <junwle2@gmail.com>
2026-07-03 12:50:20 +0900

    feature: select_stock_broker 구현

--- auto_trading_system.py
+++ auto_trading_system.py
@@ -1,2 +1,10 @@
 class AutoTradingSystem:
-    ...
+    def select_stock_broker(self, broker_name):
+        self.stock_broker = broker_name
+
+    @property
+    def stock_broker(self):
+        return self._stock_broker
+    @stock_broker.setter
+    def stock_broker(self, broker_name):
+        self._stock_broker = broker_name
