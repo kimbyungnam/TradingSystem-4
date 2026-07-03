bf957009dcda4f11cbaa743d585e162470d3818d
kimbyungnam <kimbyungnam@ajou.ac.kr>
2026-07-03 12:49:30 +0900

    [test] login

---
+++ test_auto_trading_system.py
@@ -0,0 +1,38 @@
+from pytest_mock import MockerFixture
+
+from auto_trading_system import AutoTradingSystem
+
+
+def test_select_stock_broker_kiwer():
+    # Arrange
+    sut = AutoTradingSystem()
+
+    # Action
+    sut.select_stock_broker("kiwer")
+
+    # Assert
+    assert sut.stock_broker == "kiwer"
+
+
+def test_select_stock_broker_nemom():
+    # Arrange
+    sut = AutoTradingSystem()
+
+    # Action
+    sut.select_stock_broker("nemo")
+
+    # Assert
+    assert sut.stock_broker == "nemo"
+
+
+def test_login(mocker: MockerFixture):
+    # Arrange
+    sut = AutoTradingSystem()
+    mock_brocker = mocker.patch("auto_trading_system.AutoTradingSystem.stock_brocker")
+
+    # Action
+    sut.login("ID_NOT_IMPORTANT", "PW_NOT_IMPORTANT")
+
+    # Assertion
+    mock_brocker.login.assert_called_once_with("ID_NOT_IMPORTANT", "PW_NOT_IMPORTANT")
+    pass
